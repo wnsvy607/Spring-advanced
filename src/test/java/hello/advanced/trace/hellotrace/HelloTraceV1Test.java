36d6eebf9ae8d9b2798a110501c2e82e637d70f9
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-04-26 20:00:36 +0900

    [FEAT] 로그 추적기 추가

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,33 @@
+package hello.advanced.trace.hellotrace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import hello.advanced.trace.TraceStatus;
+
+@SpringBootTest
+class HelloTraceV1Test {
+
+	@Test
+	void begin_end() {
+		HelloTraceV1 trace = new HelloTraceV1();
+		TraceStatus status = trace.begin("hello");
+		trace.end(status);
+
+	}
+
+
+	@Test
+	void begin_exception() {
+		HelloTraceV1 trace = new HelloTraceV1();
+		TraceStatus status = trace.begin("hello");
+		trace.exception(status, new IllegalArgumentException());
+	}
+
+
+
+
+
+}
