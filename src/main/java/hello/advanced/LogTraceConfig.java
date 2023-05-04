ccc970e3f1cfa87860c3a02db1847dde0ead8def
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-05-04 18:31:11 +0900

    [FEAT] 쓰레드 로컬 로그 적용

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Configuration;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
 	@Bean
 	public LogTrace logTrace() {
-		return new FieldLogTrace();
+		return new ThreadLocalLogTrace();
 	}
 }
