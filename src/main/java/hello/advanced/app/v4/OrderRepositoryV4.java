00afc7aa24122608acb7250bcbb9e3b595de6907
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-05-08 23:56:27 +0900

    [FEAT] 템플릿 메서드 패턴 적용 V4

---
+++ src/main/java/hello/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,43 @@
+package hello.advanced.app.v4;
+
+import org.springframework.stereotype.Repository;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+	private final LogTrace trace;
+
+	public void save(String itemId) {
+
+		AbstractTemplate<String> template = new AbstractTemplate<String>(trace) {
+			@Override
+			protected String call() {
+				//저장 로직
+				if (itemId.equals("ex")) {
+					throw new IllegalArgumentException("예외 발생!");
+				}
+				sleep(1000);
+				return null;
+			}
+		};
+
+		template.execute("OrderRepositoryS.save()");
+
+
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
