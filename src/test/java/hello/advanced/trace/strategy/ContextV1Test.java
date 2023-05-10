76e32343fbdcc9472768b15457bdbcaba2603375
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-05-10 18:31:27 +0900

    [FEAT] 전략 패턴 예제 2
    익명 내부 클래스, 람다식 활용

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -3,6 +3,7 @@ package hello.advanced.trace.strategy;
 import org.junit.jupiter.api.Test;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -50,6 +51,66 @@ class ContextV1Test {
 		context2.execute();
 
 	}
+
+	@Test
+	void strategyV2() {
+		Strategy strategyLogic1 = new Strategy() {
+
+			@Override
+			public void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+
+		ContextV1 context1 = new ContextV1(strategyLogic1);
+		log.info("strategyLogic1={}", strategyLogic1.getClass());
+		context1.execute();
+
+		Strategy strategyLogic2 = new Strategy() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직2 실행");
+			}
+		};
+
+		ContextV1 context2 = new ContextV1(strategyLogic2);
+		log.info("strategyLogic2={}", strategyLogic2.getClass());
+		context2.execute();
+	}
+
+	@Test
+	void strategyV3() {
+
+		ContextV1 context1 = new ContextV1(new Strategy() {
+
+			@Override
+			public void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		});
+		context1.execute();
+
+
+		ContextV1 context2 = new ContextV1(new Strategy() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직2 실행");
+			}
+		});
+		context2.execute();
+	}
+
+	@Test
+	void strategyV4() {
+
+		ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+		context1.execute();
+
+
+		ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+		context2.execute();
+	}
+
 
 
 
